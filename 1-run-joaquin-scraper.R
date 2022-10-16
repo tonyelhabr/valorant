9a2feb2bbc046b90889724093e2b3f18be051708
Tony ElHabr <anthonyelhabr@gmail.com>
2022-10-16 08:49:09 -0500

    exe tool paths

--- 1-run-joaquin-scraper.R
+++ 1-run-joaquin-scraper.R
@@ -15,7 +15,6 @@ series_ids <- series |>
   rename(event_id = eventId, start_date = startDate) |> 
   inner_join(
     events |> 
-      as_tibble() |> 
       filter(
         name |> str_detect('(Closed|Open) Qualifier', negate = TRUE)
       ) |> 
@@ -33,30 +32,30 @@ series_ids <- series |>
     team2_name
   )
 
-tool_dir <- file.path(getwd(), "src")
+tool_dir <- file.path(getwd(), 'src')
 setwd(tool_dir)
 scrape_series <- function(series_id) {
-  output_dir <- file.path(tool_dir, "data", series_id)
+  output_dir <- file.path('../data', series_id)
   if (fs::dir_exists(output_dir)) {
-    message(sprintf("Returning early for %s.", series_id))
+    message(sprintf('Returning early for %s.', series_id))
     return(NULL)
   }
-  txt_path <- file.path(tool_dir, "data.txt")
-  data_dir <- file.path(tool_dir, "data-pulled")
+  txt_path <- file.path(tool_dir, 'data.txt')
+  data_dir <- file.path(tool_dir, 'data-pulled')
+  on.exit(fs::file_delete(txt_path))
+  on.exit(fs::dir_delete(data_dir))
 
   fs::dir_create(data_dir)
-  write_lines(sprintf("https://rib.gg/series/%s", series_id), txt_path)
-  res <- system2("webscrape.exe")
+  write_lines(sprintf('https://rib.gg/series/%s', series_id), txt_path)
+  res <- system2('webscrape.exe')
   fs::dir_copy(
     data_dir,
     output_dir
   )
-  fs::file_delete(txt_path)
-  fs::dir_delete(data_dir)
   invisible(res)
 }
 
 series_ids |> 
-  # filter(region_id == 7) |> 
+  filter(region_id == 1) |> 
   pull(id) |> 
   walk(scrape_series)
